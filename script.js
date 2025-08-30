8dea29b0abe26b32a06a3c1180f71a4b277f6835
DevNazCz <nazar.chudy@gmail.com>
2025-08-30 20:03:43 +0200

    Registration page
    Added registration page in index.html and script.js

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
+// A button that scrolls the page up
+
 const scrollButton = document.getElementById("scroll-btn");
 
 window.addEventListener("scroll", () => {
   scrollButton.style.display = scrollY <= 250 ? "none" : "block";
 });
+
+// Function to validate password confirmation
+
+const firstPass = document.getElementById("input-password");
+const secondPass = document.getElementById("input-second-password");
+
+const validatePasswords = () => {
+  if (firstPass.value === "" || secondPass.value === "") {
+    firstPass.style.borderColor = "";
+    secondPass.style.borderColor = "";
+    return;
+  }
+
+  if (firstPass.value !== secondPass.value) {
+    firstPass.style.borderColor = "red";
+    secondPass.style.borderColor = "red";
+  } else {
+    firstPass.style.borderColor = "green";
+    secondPass.style.borderColor = "green";
+  }
+};
+
+firstPass.addEventListener("input", validatePasswords);
+secondPass.addEventListener("input", validatePasswords);
